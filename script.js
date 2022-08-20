dd20d607e9089b38f2c02a45c6feeb40d4d87d07
Saumya-1008 <104722509+Saumya-1008@users.noreply.github.com>
2022-08-21 00:20:21 +0530

    modifiedjs

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let searchBtn =document.querySelector('#search-btn');
 let searchBar =document.querySelector('.search-bar-container');
+
+
 searchBtn.addEventListener('click' ,() =>{
 searchBtn.classList.toggle('fa-times');
-searchBtn.classList.toggle('active');
+searchBar.classList.toggle('active');
 
 });
